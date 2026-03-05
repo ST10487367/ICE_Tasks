6cbec0ab40ce5cf9c1e8a5f4f91f67e571c6047f
ST10487367_Shalin_Sing_BCA1_PROG5112_GRO3 <ST10487367@myemeris.edu.za>
2026-03-05 10:45:30 +0200

    ICE Task 1

---
+++ MovieRatingApp/Data/AppDBContent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieRatingApp.Models;
+
+namespace MovieRatingApp.Data
+{
+    public class AppDbContent : DbContext
+    {
+        public AppDbContent(DbContextOptions<AppDbContent> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
